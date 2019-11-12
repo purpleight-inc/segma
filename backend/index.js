e3d2506a66d8f10b9b513145e902453374a71296
Amine BAMBRIK <amine.bambrik.development@gmail.com>
2019-11-12 06:33:37 +0100

    Setup mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const debug = require("debug")("APP:SERVER");
 const config = require("config");
 const express = require("express");
 const http = require("http"); 
+const mongoose = require("mongoose");
+
+mongoose.connect(config.get("mongoose.url"),config.get("mongoose.options"),()=>{
+    debug(`mongoose connected at ${config.get("mongoose.url")}`)
+});
+mongoose.set('useCreateIndex', true);
 
 const app = express();
 
@@ -11,6 +17,7 @@ app.use(express.urlencoded({
 }));
 
 app.set("port",process.env.PORT || config.get("port") );
+
 
 http.createServer(app).listen(app.get("port"),()=>{
     debug(`${app.get('env')} server Started at port ${app.get("port")};Press Ctrl + C to stop it`);
